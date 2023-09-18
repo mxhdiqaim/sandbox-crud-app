cccc47eb04dca829f6583c8056361ebb0f9e0ab1
Mahdi <muhdmahdia36@gmail.com>
2023-09-18 16:02:49 +0100

    Detecting production / development env

--- server/server.js
+++ server/server.js
@@ -19,8 +19,6 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(express.json({ limit: "50mb" }));
 
-dotenv.config();
-
 // disable cors
 app.use(cors());
 
@@ -28,7 +26,15 @@ app.use(cors());
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 
-app.use("/upload", express.static("upload"));
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
